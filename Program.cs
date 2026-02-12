85d0b0c97ed85330bced1eb46485014d5788ab2d
vsaybuu@bk.ru <vsaybuu@bk.ru>
2026-02-12 15:48:45 +0700

    код исправлен

--- Program.cs
+++ Program.cs
@@ -2,28 +2,38 @@
 
 switch (Console.ReadLine())  {
   case "1":
-    Console.Write(" Base number = ");
-    int baseValue = Convert.ToInt32(Console.ReadLine());
-    Console.Write(" Exponent = ");
-    int powerValue = Convert.ToInt32(Console.ReadLine());
+    int baseValue;
+    int powerValue;
 
-    int calculationResult = 1;
+    Console.Write(" Base number = ");
+    baseValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write(" Exponent = ");
+    powerValue = Convert.ToInt32(Console.ReadLine());
+
+    int calculationResult;
+    calculationResult = 1;
 
     for (int loopCounter = 0; loopCounter < powerValue; ++loopCounter)
     {
       calculationResult *= baseValue;
     }
+    
     Console.WriteLine($" Power = {baseValue}^{powerValue} = {calculationResult} ");
+    
     break;
 
   case "2":
+    string userInput, transformedNumber;
+    char middleDigit;
+
     Console.Write(" Number(>= 100) = ");
-    string userInput = Console.ReadLine();
+    userInput = Console.ReadLine();
                 
-    char middleDigit = userInput[1];
-    string transformedNumber = userInput[0] + userInput.Substring(2) + middleDigit;
+    middleDigit = userInput[1];
+    transformedNumber = userInput[0] + userInput.Substring(2) + middleDigit;
 
     Console.WriteLine($" Result = {transformedNumber} ");
+
     break;
 }
 
